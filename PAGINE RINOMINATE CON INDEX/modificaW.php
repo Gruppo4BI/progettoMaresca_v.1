d12a748fd49a7e248f4c8972b3abb1d8c7c8ba92
dariotr <gaskdfire@gmail.com>
2016-04-11 12:14:31 +0200

    Update modificaW.php
    Ho aggiusto un controllo per evitare che qualcuno acceda senza essers loggiato.
    Aggiusto gli spazi del campo Maggiore informazioni. Messi controlli con pattern su telefono,telefono2,fax e cellulare. C'è un controllo per accertarsi che il campo telefono sia unico del database

--- PAGINE RINOMINATE CON INDEX/modificaW.php
+++ PAGINE RINOMINATE CON INDEX/modificaW.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+// controllo se ¬è stato impostato l id per evitare che accedano direttamente senza fare il login
+if( !isset($_SESSION['id']) )
+	header("Location: index.php");
 ?>
 
 <!DOCTYPE html>
@@ -12,24 +15,28 @@ session_start();
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-    <link href="css/menu.css" rel="stylesheet" type="text/css">
+    <link href="menu.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <?php
 $nl="<br/>";
 include_once 'configurazioneDB.php';
+
 // ricavo tutte le informazioni che mi servono sull azienda tramite l id che p una variabile di sessione
 $comando= "select * from azienda where id='".$_SESSION['id']."'";
 $result = $conn->query($comando);
 $dati = $result->fetch_assoc();
+
 //ricavo attraverso l idCat il nome della categoria della mia azienda
 $SQLcateg ="select categoria from categoria where idCat='".$dati['idCat']."'";
 $result_cat = $conn->query($SQLcateg);
 $dati_cat=$result_cat->fetch_assoc();
-// il nome sarË† memorizzato nella variabile categoria
+// il nome sarÀÜ memorizzato nella variabile categoria
 $categoria=$dati_cat['categoria'];
-// questa Â un altra query che mi serve per pooi avere tutta la lista di categorie senza quella che  stata selezionata precedentemente
+
+// questa ¬è un altra query che mi serve per pooi avere tutta la lista di categorie senza quella che è stata selezionata precedentemente
 $comandoSQL ="select categoria from categoria where categoria !='".$categoria."'";
+
 		$result=mysqli_query($conn,$comandoSQL); 
 		
 // ricavo le informazioni sui telefoni della mia azienda tramite idTel ottenuto con la prima query
@@ -38,11 +45,11 @@ $result_tel = $conn->query($SQLtel);
 $dati_tel=$result_tel->fetch_assoc();
 ?>
 <!-- sono classi predefinite, inverse vuol dire sfondo scuro
-	quella di default Â chiara -->
+	quella di default ¬è chiara -->
 <nav class="navbar navbar-default">
   <div class="container-fluid">
   
-  <!-- questo Â l header contiene gli elementi che devono essere visibili anche quando la barra Â minimizzata per i display di piccole dimensioni -->
+  <!-- questo ¬è l header contiene gli elementi che devono essere visibili anche quando la barra ¬è minimizzata per i display di piccole dimensioni -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
@@ -50,7 +57,7 @@ $dati_tel=$result_tel->fetch_assoc();
         <span class="icon-bar"></span>   
         <!-- Ciascuno di questi disegna una lineetta sul pulsante quando si minimizza la pagina -->                     
       </button>
-      <a class="navbar-brand" href=" ">Borsa delle Idee</a>
+      <a class="navbar-brand" href="">Borsa delle Idee</a>
     </div>
  <!--  elementi della barra -->
     <div class="collapse navbar-collapse" id="myNavbar">
@@ -81,12 +88,29 @@ $dati_tel=$result_tel->fetch_assoc();
     <div class="col-sm-offset-2 col-sm-8">
       <div class="well well-lg">
         <h4>Modifica i tuoi dati</h4>
-           <!-- Questo  un form con tutti i dati che sarˆ possibile modificare, utilizzeremo il metodo post -->
+        
+        <?php 
+// se emodifica.php mi da qualche errore, compariranno dei messaggi di avvertimento, con lo switch gestisco i var casi
+if( isset($_GET['errore']) )
+{
+	echo "<p id='box_errore'> <div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+    <strong>Attenzione!</strong> ";
+	switch ($_GET['errore'])
+	{
+	case 1:
+		echo "Numero di telefono è già presente";
+		break;
+
+		}
+		echo "</div>";
+}
+?> 
+           <!-- Questo ¬è un form con tutti i dati che sarÀÜ possibile modificare, utilizzeremo il metodo post -->
   <form method="post" name="registra" action="emodifica.php" id="registra">
  
   <fieldset class="form-group">
-    <label for="email">Email</label>
-    <input type="email" class="form-control" id="email" name="email" value='<?php echo $dati["email"]; ?>' >
+    <label for="Nome">Nome Azienda</label>
+    <input type="nome" class="form-control" id="nome" name="nome" value='<?php echo $dati["nome_azienda"]; ?>' >
   </fieldset>
   <fieldset class="form-group">
     <label for="cap">Cap</label>
@@ -116,22 +140,22 @@ $dati_tel=$result_tel->fetch_assoc();
   
    <fieldset class="form-group">
     <label for="numero">Telefono </label>
-    <input type="text" pattern="[0-9]{1,15}"class="form-control" id="numero" name="numero" value='<?php echo $dati_tel["numero"]; ?>'>
+    <input type="tel" pattern="[0-9]{1,15}" required class="form-control" id="numero" name="numero" value='<?php echo $dati_tel["numero"]; ?>'>
   </fieldset>
   
     <fieldset class="form-group">
     <label for="numero2">Telefono 2 </label>
-    <input type="text" pattern="[0-9]{1,15}" class="form-control" id="numero2" name="numero2" value='<?php echo $dati_tel["numero2"]; ?>'>
+    <input type="tel" pattern="[0-9]{1,15}" class="form-control" id="numero2" name="numero2" value='<?php echo $dati_tel["numero2"]; ?>'>
   </fieldset>
   
      <fieldset class="form-group">
     <label for="fax">Fax </label>
-    <input type="text" pattern="[0-9]{1,15}" class="form-control" id="fax" name="fax" value='<?php echo $dati_tel["fax"]; ?>'>
+   <input type="tel" pattern="[0-9]{1,15}"  class="form-control" id="fax" name="fax" value='<?php echo $dati_tel["fax"]; ?>'>
   </fieldset>
   
        <fieldset class="form-group">
     <label for="cellulare">Cellulare</label>
-    <input type="text" pattern="[0-9]{1,15}" class="form-control" id="cellulare" name="cellulare" value='<?php echo $dati_tel["cellulare"]; ?>'>
+     <input type="tel" pattern="[0-9]{1,15}" class="form-control" id="cellulare" name="cellulare" value='<?php echo $dati_tel["cellulare"]; ?>'>
   </fieldset>
   
   <fieldset class="form-group">
@@ -148,10 +172,10 @@ $dati_tel=$result_tel->fetch_assoc();
   
   <fieldset class="form-group">
     <label for="parlaci">Maggiori dettagli</label>
-    <textarea class="form-control" id="parlaci" name="parlaci" rows="3"> <?php echo $dati["parlaci"]; ?> </textarea>
+    <textarea class="form-control" id="parlaci" name="parlaci" rows="3"><?php echo $dati["parlaci"];?></textarea>
   </fieldset>
   
-<!-- questo potrà essere aggiunto dopo per caricare l immagine dell azienda 
+<!-- questo potr√† essere aggiunto dopo per caricare l immagine dell azienda 
   <fieldset class="form-group">
     <label for="exampleInputFile">File input</label>
     <input type="file" class="form-control-file" id="exampleInputFile">
@@ -164,6 +188,9 @@ $dati_tel=$result_tel->fetch_assoc();
 <br/> <p style="text-align: right"><a href='Menu.php'><button> Indietro </button></a></p>
   </div>
 </div>
+
+</body>
+</html>
 
 </body>
 </html>
